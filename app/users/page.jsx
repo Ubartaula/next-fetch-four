95862f8cf066c255f4332370bd93f9c087de0315
Umesh <bartaula.umeshkumar@gmail.com>
2024-05-16 02:33:24 -0500

    putting back

--- app/users/page.jsx
+++ app/users/page.jsx
@@ -3,18 +3,18 @@ import { getUsers } from "@/utils/getUsers";
 import React, { Suspense } from "react";
 
 const UsersPage = async () => {
-  // const res = await fetch(`${process.env.NEXT_AUTH_URL}/api/users`, {
+  const res = await fetch(`${process.env.NEXT_AUTH_URL}/api/users`, {
+    // headers: {
+    //   "Content-Type": "application/json",
+    // },
+    cache: "no-store",
+  });
+  // const res = await fetch(`/api/users`, {
   //   headers: {
   //     "Content-Type": "application/json",
   //   },
   //   cache: "no-store",
   // });
-  const res = await fetch(`/api/users`, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    cache: "no-store",
-  });
 
   const users = await res.json();
 
